fd0544036809c49ac7ab6c22b291d7d2bf0e85a7
Akshat <akshatgupta0302@gmail.com>
2026-01-01 14:06:50 +0530

    Name update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,7 +15,7 @@ const NavBar = () => {
         <div className="flex justify-between items-center">
           <div className="flex items-center flex-shrink-0">
             <img className="h-10 w10 mr-2" src={logo} alt="Logo" />
-            <span className="text-xl tracking-tight">VirtualR</span>
+            <span className="text-xl tracking-tight">VirtualVR</span>
           </div>
           <ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
